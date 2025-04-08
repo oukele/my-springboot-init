3a4209533cb3e4634e3dab8333840968d0f14ec6
oukele <2668697130@qq.com>
2025-04-08 17:47:05 +0800

    优化

--- src/main/java/com/oukele/springbootinit/utils/SpringContextUtils.java
+++ src/main/java/com/oukele/springbootinit/utils/SpringContextUtils.java
@@ -1,6 +1,5 @@
 package com.oukele.springbootinit.utils;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -17,7 +16,7 @@ public class SpringContextUtils implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         SpringContextUtils.applicationContext = applicationContext;
     }
 
